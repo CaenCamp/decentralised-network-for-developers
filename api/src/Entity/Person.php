778958e2c90e6d22d01b6fd7d545a9719e372b3e
Alexis Janvier <contact@alexisjanvier.net>
2020-08-07 17:05:51 +0200

    Improve Person WIP

--- api/src/Entity/Person.php
+++ api/src/Entity/Person.php
@@ -30,6 +30,42 @@ class Person
     private $id;
 
     /**
+     * @var string|null an additional name for a Person, can be used for a middle name
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/additionalName")
+     * @Assert\Type(type="string")
+     */
+    private $additionalName;
+
+    /**
+     * @var string|null Family name. In the U.S., the last name of an Person. This can be used along with givenName instead of the name property.
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/familyName")
+     * @Assert\Type(type="string")
+     */
+    private $familyName;
+
+    /**
+     * @var string|null Given name. In the U.S., the first name of a Person. This can be used along with familyName instead of the name property.
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/givenName")
+     * @Assert\Type(type="string")
+     */
+    private $givenName;
+
+    /**
+     * @var string|null an honorific prefix preceding a Person's name such as Dr/Mrs/Mr
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/honorificPrefix")
+     * @Assert\Type(type="string")
+     */
+    private $honorificPrefix;
+
+    /**
      * @var string|null a description of the item
      *
      * @ORM\Column(type="text", nullable=true)
@@ -39,13 +75,13 @@ class Person
     private $description;
 
     /**
-     * @var string|null the name of the item
+     * @var string|null A sub property of description. A short description of the item used to disambiguate from other, similar items. Information from other properties (in particular, name) may be necessary for the description to be useful for disambiguation.
      *
      * @ORM\Column(type="text", nullable=true)
-     * @ApiProperty(iri="http://schema.org/name")
+     * @ApiProperty(iri="http://schema.org/disambiguatingDescription")
      * @Assert\Type(type="string")
      */
-    private $name;
+    private $disambiguatingDescription;
 
     /**
      * @var string|null An image of the item. This can be a \[\[URL\]\] or a fully described \[\[ImageObject\]\].
@@ -65,9 +101,65 @@ class Person
      */
     private $url;
 
+    /**
+     * @var Organization|null an Organization (or ProgramMembership) to which this Person or Organization belongs
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\Organization")
+     * @ApiProperty(iri="http://schema.org/memberOf")
+     */
+    private $memberOf;
+
+    /**
+     * @var Event|null event that this person is a performer or participant in
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\Event")
+     * @ApiProperty(iri="http://schema.org/performerIn")
+     */
+    private $performerIn;
+
     public function getId(): ?string
     {
         return $this->id;
+    }
+
+    public function setAdditionalName(?string $additionalName): void
+    {
+        $this->additionalName = $additionalName;
+    }
+
+    public function getAdditionalName(): ?string
+    {
+        return $this->additionalName;
+    }
+
+    public function setFamilyName(?string $familyName): void
+    {
+        $this->familyName = $familyName;
+    }
+
+    public function getFamilyName(): ?string
+    {
+        return $this->familyName;
+    }
+
+    public function setGivenName(?string $givenName): void
+    {
+        $this->givenName = $givenName;
+    }
+
+    public function getGivenName(): ?string
+    {
+        return $this->givenName;
+    }
+
+    public function setHonorificPrefix(?string $honorificPrefix): void
+    {
+        $this->honorificPrefix = $honorificPrefix;
+    }
+
+    public function getHonorificPrefix(): ?string
+    {
+        return $this->honorificPrefix;
     }
 
     public function setDescription(?string $description): void
@@ -80,14 +172,14 @@ class Person
         return $this->description;
     }
 
-    public function setName(?string $name): void
+    public function setDisambiguatingDescription(?string $disambiguatingDescription): void
     {
-        $this->name = $name;
+        $this->disambiguatingDescription = $disambiguatingDescription;
     }
 
-    public function getName(): ?string
+    public function getDisambiguatingDescription(): ?string
     {
-        return $this->name;
+        return $this->disambiguatingDescription;
     }
 
     public function setImage(?string $image): void
@@ -108,5 +200,25 @@ class Person
     public function getUrl(): ?string
     {
         return $this->url;
+    }
+
+    public function setMemberOf(?Organization $memberOf): void
+    {
+        $this->memberOf = $memberOf;
+    }
+
+    public function getMemberOf(): ?Organization
+    {
+        return $this->memberOf;
+    }
+
+    public function setPerformerIn(?Event $performerIn): void
+    {
+        $this->performerIn = $performerIn;
+    }
+
+    public function getPerformerIn(): ?Event
+    {
+        return $this->performerIn;
     }
 }
--- doc/notes.md
+++ doc/notes.md
@@ -52,3 +52,50 @@ Et puis pour le "lancement du projet"
 2) Ajout de la conf des uuid dans le schema.yaml
 
 It's working \o/
+
+## Amélioration du modèle
+
+Pour le moment, en schema.json bête et méchant :
+
+* CreativeWork
+* Event
+* MediaObject
+* Organization
+* Person
+* Place
+* PostalAddress
+
+Pour coller aux données existantes et au "métier" des CCC, on pourrait plutôt avoir :
+
+### Un event / un évènement
+
+Donc un event pourrait avoir un 
+* type (talk, lightning, coding ...)
+* un ou plusieurs speakers de type Person
+* un lieu / Place
+* un organisateur (CaenCamp / Devops CaenCamp / Coding CaenCamp)
+
+Mais en vrai, on devrait avoir une distinction entre l'évènement et les sessions qu'il contient. Par exemple pour une édition des CaenCamp, on doit pouvoir avoir un Talk et un lightning ! => Fouiller les sur/sous évènements de schema.org !
+
+On doit aussi pouvoir associer une ou plusieurs photos et une ou plusieurs vidéos à un event. En fait, plusieur CreativeWork qui pourraient-être de plusieurs types (audio - vidéos - photos - slides ...)
+
+### Une organization / Boite
+
+Une personne peut travailler pour une boite
+Une boite peut être sponsor
+Une boite peut prêter un lieu (Place)
+
+### Une personne
+
+Une personne peut travailler dans une boite
+Une personne peut faire un event (sous-event)
+
+### Des médias
+
+CreativeWork associés à des events
+
+### Un lieu
+
+Un lieu correspond à un lieux physique et donc à une adresse postale.
+Un lieu peut accueillir un évènement en tant que tel, mais un lieu peut aussi être associer à une boite.
+
